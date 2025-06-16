2154317bc38dae16299ae2e7de21e02c10fafa5d
TalalAlrashedi <TalalAlrashedi24@hotmail.com>
2025-06-16 13:02:49 +0300

    remove node_modules

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const https = require("https");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.redirect(303, "/hello-world");
+});
+
+app.get("/hello-world", (req, res) => {
+  res.redirect(303, "/hello-world.json");
+});
+
+app.get("/hello-world.json", (req, res) => {
+  res.json({ message: "Hello, world!" });
+});
+
+app.get("/image", (req, res) => {
+  res.redirect(
+    303,
+    "https://images.pexels.com/photos/31285211/pexels-photo-31285211/free-photo-of-serene-landscape-of-scottish-highlands.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+  );
+});
+
+app.use((req, res) => {
+  res.status(405).send(`${req.method} is not supported on ${req.path}`);
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
